238c7a13c718524177bd82bedfc6aad82b4af9b3
chingweich <cat8007@gamil.com>
2015-09-15 23:56:49 +0800

    first commit

---
+++ HHbbbbAnalyzer.C
@@ -0,0 +1,111 @@
+#include <TLegend.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <TH1D.h>
+#include <TRandom.h>
+#include <TLorentzVector.h>
+#include <TFile.h>
+#include <TGraph.h>
+#include <TGraphErrors.h>
+#include <TH1F.h>
+#include <TH1.h>
+#include <TCanvas.h>
+#include <TROOT.h>
+#include "TImage.h"
+#include "TSystem.h"
+#include "TStyle.h"
+#include "untuplizer.h"
+#include <TClonesArray.h>
+#include <fstream>
+#include <cmath>
+#include <TSystem.h>
+#include <string>
+#include <sstream>
+
+TFile *f;
+
+void HHbbbbAnalyzer(){
+	string  masspoint[11]={"1000","1200","1400","1600","1800","2000","2500","3000","3500","4000","4500"};
+	for (int massP=0;massP<11;massP++){
+		
+		//signal
+		f = TFile::Open(Form("/data2/syu/13TeV/BulkGravTohhTohbbhbb/softdrop_BulkGravTohhTohbbhbb_narrow_M-%s_13TeV-madgraph.root",masspoint[massP].data()));
+		if (!f || !f->IsOpen())continue;
+		TDirectory * dir = (TDirectory*)f->Get(Form("/data2/syu/13TeV/BulkGravTohhTohbbhbb/softdrop_BulkGravTohhTohbbhbb_narrow_M-%s_13TeV-madgraph.root:/tree",masspoint[massP].data()));
+		dir->GetObject("treeMaker",tree);
+		
+		TreeReader data(tree);
+		
+		for(Long64_t jEntry=0; jEntry<data.GetEntriesFast() ;jEntry++){
+			data.GetEntry(jEntry);
+			
+			Int_t nJet         = data.GetInt("FATnJet");
+			TClonesArray* FATjetP4 = (TClonesArray*) data.GetPtrTObject("FATjetP4");
+			float*   FATjetPRmass= data.GetPtrFloat("FATjetPRmass");  
+			vector<int> FatjetPreSelection;
+			for(int i=0;i<nJet;i++){
+				if(FATjetPRmass[i]>135||FATjetPRmass[i]<110)continue;
+				TLorentzVector* thisHiggs = (TLorentzVector*)FATjetP4->At(i);
+				if(thisHiggs.Pt()<30)continue;
+				if(thisHiggs.Eta()>2.5)continue;
+				
+				FatjetPreSelection.push_back(i);
+			}
+			
+			float*  FATjetCISVV2 = data.GetPtrFloat("FATjetCISVV2"); 
+			float* FATjetTau1=data.GetPtrFloat("FATjetTau1");
+		    float* FATjetTau2=data.GetPtrFloat("FATjetTau2");
+			Int_t* FATnSubSDJet=data.GetPtrInt("FATnSubSDJet");
+			vector<float>   *FATsubjetSDCSV = data.GetPtrVectorFloat("FATsubjetSDCSV");
+		    vector<float>   *FATsubjetSDPx =  data.GetPtrVectorFloat("FATsubjetSDPx");
+		    vector<float>   *FATsubjetSDPy =  data.GetPtrVectorFloat("FATsubjetSDPy");
+		    vector<float>   *FATsubjetSDPz =  data.GetPtrVectorFloat("FATsubjetSDPz");
+		    vector<float>   *FATsubjetSDCE =  data.GetPtrVectorFloat("FATsubjetSDCE");
+			
+			int thisHiggsNum=0,thatHiggsNum=0;
+			
+			for(int i=0;i<FatjetPreSelection.size();i++){
+				if(FATnSubSDJet[FatjetPreSelection[i]]<2)continue;
+				for(int j=i+1;j<FatjetPreSelection.size();j++){
+					TLorentzVector* thisHiggs = (TLorentzVector*)FATjetP4->At(FatjetPreSelection[i]);
+					TLorentzVector* thatHiggs = (TLorentzVector*)FATjetP4->At(FatjetPreSelection[j]);
+					TLorentzVector* mjj;
+					mjj= thisHiggs+thatHiggs;
+					if(mjj.M()<890)continue;
+					if(thisHiggs->DeltaPhi(*thatHiggs)<1.3)continue;
+					if(FATnSuibSDJet[FatjetPreSelection[j]]<2)continue;
+					TLorentzVector  FATsubjet_1,FATsubjet_2;
+					FATsubjet_1.SetPxPyPzE(FATsubjetSDPx[FatjetPreSelection[i]][0],FATsubjetSDPy[FatjetPreSelection[i]][0],FATsubjetSDPz[FatjetPreSelection[i]][0],FATsubjetSDCE[FatjetPreSelection[i]][0]);
+					FATsubjet_2.SetPxPyPzE(FATsubjetSDPx[FatjetPreSelection[i]][1],FATsubjetSDPy[FatjetPreSelection[i]][1],FATsubjetSDPz[FatjetPreSelection[i]][1],FATsubjetSDCE[FatjetPreSelection[i]][1]);
+					bool subjet1PassCSV=1,subjet2PassCSV=1;
+					if(FATsubjet_1->DeltaR(*FATsubjet_2)<0.3 && FATjetCISVV2[FatjetPreSelection[i]]<0.244)subjet1PassCSV=0;
+					if(FATsubjet_1->DeltaR(*FATsubjet_2)>0.3 && (FATsubjetSDCSV[FatjetPreSelection[i]][0]<0.244 || FATsubjetSDCSV[FatjetPreSelection[i]][1]<0.244))subjet1PassCSV=0;
+					
+					
+					FATsubjet_1.SetPxPyPzE(FATsubjetSDPx[FatjetPreSelection[j]][0],FATsubjetSDPy[FatjetPreSelection[j]][0],FATsubjetSDPz[FatjetPreSelection[j]][0],FATsubjetSDCE[FatjetPreSelection[j]][0]);
+					FATsubjet_2.SetPxPyPzE(FATsubjetSDPx[FatjetPreSelection[j]][1],FATsubjetSDPy[FatjetPreSelection[j]][1],FATsubjetSDPz[FatjetPreSelection[j]][1],FATsubjetSDCE[FatjetPreSelection[j]][1]);
+					if(FATsubjet_1->DeltaR(*FATsubjet_2)<0.3 && FATjetCISVV2[FatjetPreSelection[i]]<0.244)subjet2PassCSV=0;
+					if(FATsubjet_1->DeltaR(*FATsubjet_2)>0.3 && (FATsubjetSDCSV[FatjetPreSelection[i]][0]<0.244 || FATsubjetSDCSV[FatjetPreSelection[i]][1]<0.244))subjet2PassCSV=0;
+					if(subjet1PassCSV==0 &&  subjet2PassCSV==0 )continue;
+					
+					thisHiggsNum=FatjetPreSelection[i];
+					thatHiggsNum=FatjetPreSelection[j];
+					break;
+					
+					
+					
+				}
+				if(thisHiggsNum!=0)break;
+			}
+			
+			
+			
+			
+		}
+		
+		
+		
+	}
+	
+}
