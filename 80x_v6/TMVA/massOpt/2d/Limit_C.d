fbe94749973001f5d53c6c916621f041fb46e64a
chingweich <cat8007@gmail.com>
2016-11-23 23:37:44 +0800

    mass opt v0

---
+++ 80x_v6/TMVA/massOpt/2d/Limit_C.d
@@ -0,0 +1,123 @@
+
+# DO NOT DELETE
+
+./Limit_C.so: /home/neutron/root/include/TLegend.h
+./Limit_C.so: /home/neutron/root/include/TPave.h
+./Limit_C.so: /home/neutron/root/include/TBox.h
+./Limit_C.so: /home/neutron/root/include/TObject.h
+./Limit_C.so: /home/neutron/root/include/Rtypes.h
+./Limit_C.so: /home/neutron/root/include/RtypesCore.h
+./Limit_C.so: /home/neutron/root/include/RConfig.h
+./Limit_C.so: /home/neutron/root/include/RVersion.h
+./Limit_C.so: /home/neutron/root/include/DllImport.h
+./Limit_C.so: /home/neutron/root/include/Rtypeinfo.h
+./Limit_C.so: /usr/include/c++/6/typeinfo
+./Limit_C.so: /home/neutron/root/include/snprintf.h
+./Limit_C.so: /home/neutron/root/include/strlcpy.h /usr/include/c++/6/atomic
+./Limit_C.so: /home/neutron/root/include/TGenericClassInfo.h
+./Limit_C.so: /usr/include/c++/6/vector
+./Limit_C.so: /home/neutron/root/include/TSchemaHelper.h
+./Limit_C.so: /usr/include/c++/6/string /home/neutron/root/include/TStorage.h
+./Limit_C.so: /home/neutron/root/include/TVersionCheck.h
+./Limit_C.so: /home/neutron/root/include/Riosfwd.h /usr/include/c++/6/iosfwd
+./Limit_C.so: /home/neutron/root/include/TBuffer.h
+./Limit_C.so: /home/neutron/root/include/TAttLine.h
+./Limit_C.so: /home/neutron/root/include/TAttFill.h
+./Limit_C.so: /home/neutron/root/include/TAttBBox2D.h
+./Limit_C.so: /home/neutron/root/include/GuiTypes.h
+./Limit_C.so: /home/neutron/root/include/TPoint.h
+./Limit_C.so: /home/neutron/root/include/TString.h
+./Limit_C.so: /home/neutron/root/include/TMathBase.h
+./Limit_C.so: /usr/include/c++/6/cstdlib /usr/include/c++/6/cmath
+./Limit_C.so: /home/neutron/root/include/RStringView.h
+./Limit_C.so: /home/neutron/root/include/RConfigure.h
+./Limit_C.so: /home/neutron/root/include/RWrap_libcpp_string_view.h
+./Limit_C.so: /usr/include/c++/6/algorithm /usr/include/c++/6/iterator
+./Limit_C.so: /usr/include/c++/6/ostream /usr/include/c++/6/iomanip
+./Limit_C.so: /usr/include/c++/6/stdexcept
+./Limit_C.so: /home/neutron/root/include/libcpp_string_view.h
+./Limit_C.so: /home/neutron/root/include/TAttText.h
+./Limit_C.so: /usr/include/c++/6/iostream /home/neutron/root/include/TH1D.h
+./Limit_C.so: /home/neutron/root/include/TH1.h
+./Limit_C.so: /home/neutron/root/include/TAxis.h
+./Limit_C.so: /home/neutron/root/include/TNamed.h
+./Limit_C.so: /home/neutron/root/include/TAttAxis.h
+./Limit_C.so: /home/neutron/root/include/TArrayD.h
+./Limit_C.so: /home/neutron/root/include/TArray.h
+./Limit_C.so: /home/neutron/root/include/TAttMarker.h
+./Limit_C.so: /home/neutron/root/include/TArrayC.h
+./Limit_C.so: /home/neutron/root/include/TArrayS.h
+./Limit_C.so: /home/neutron/root/include/TArrayI.h
+./Limit_C.so: /home/neutron/root/include/TArrayF.h
+./Limit_C.so: /home/neutron/root/include/Foption.h
+./Limit_C.so: /home/neutron/root/include/TVectorFfwd.h
+./Limit_C.so: /home/neutron/root/include/TVectorDfwd.h
+./Limit_C.so: /home/neutron/root/include/TFitResultPtr.h
+./Limit_C.so: /usr/include/c++/6/memory /home/neutron/root/include/TH2D.h
+./Limit_C.so: /home/neutron/root/include/TH2.h
+./Limit_C.so: /home/neutron/root/include/TMatrixFBasefwd.h
+./Limit_C.so: /home/neutron/root/include/TMatrixDBasefwd.h
+./Limit_C.so: /home/neutron/root/include/TRandom.h
+./Limit_C.so: /home/neutron/root/include/TLorentzVector.h
+./Limit_C.so: /home/neutron/root/include/TMath.h
+./Limit_C.so: /home/neutron/root/include/TError.h /usr/include/c++/6/limits
+./Limit_C.so: /home/neutron/root/include/TVector3.h
+./Limit_C.so: /home/neutron/root/include/TVector2.h
+./Limit_C.so: /home/neutron/root/include/TMatrix.h
+./Limit_C.so: /home/neutron/root/include/TMatrixF.h
+./Limit_C.so: /home/neutron/root/include/TMatrixT.h
+./Limit_C.so: /home/neutron/root/include/TMatrixTBase.h
+./Limit_C.so: /home/neutron/root/include/TMatrixTUtils.h
+./Limit_C.so: /home/neutron/root/include/TMatrixFfwd.h
+./Limit_C.so: /home/neutron/root/include/TMatrixFUtils.h
+./Limit_C.so: /home/neutron/root/include/TMatrixFUtilsfwd.h
+./Limit_C.so: /home/neutron/root/include/TRotation.h
+./Limit_C.so: /home/neutron/root/include/TFile.h
+./Limit_C.so: /home/neutron/root/include/TDirectoryFile.h
+./Limit_C.so: /home/neutron/root/include/TDirectory.h
+./Limit_C.so: /home/neutron/root/include/TList.h
+./Limit_C.so: /home/neutron/root/include/TSeqCollection.h
+./Limit_C.so: /home/neutron/root/include/TCollection.h
+./Limit_C.so: /home/neutron/root/include/TIterator.h
+./Limit_C.so: /home/neutron/root/include/TDatime.h
+./Limit_C.so: /home/neutron/root/include/TUUID.h
+./Limit_C.so: /home/neutron/root/include/TMap.h
+./Limit_C.so: /home/neutron/root/include/THashTable.h
+./Limit_C.so: /home/neutron/root/include/TUrl.h
+./Limit_C.so: /home/neutron/root/include/TGraph.h
+./Limit_C.so: /home/neutron/root/include/TGraphErrors.h
+./Limit_C.so: /home/neutron/root/include/TGraphAsymmErrors.h
+./Limit_C.so: /home/neutron/root/include/TH1F.h
+./Limit_C.so: /home/neutron/root/include/TCanvas.h
+./Limit_C.so: /home/neutron/root/include/TPad.h
+./Limit_C.so: /home/neutron/root/include/TVirtualPad.h
+./Limit_C.so: /home/neutron/root/include/TAttPad.h
+./Limit_C.so: /home/neutron/root/include/TVirtualX.h
+./Limit_C.so: /usr/include/c++/6/utility /home/neutron/root/include/Buttons.h
+./Limit_C.so: /home/neutron/root/include/TQObject.h
+./Limit_C.so: /home/neutron/root/include/TQObjectEmitVA.h
+./Limit_C.so: /home/neutron/root/include/TQConnection.h
+./Limit_C.so: /home/neutron/root/include/Varargs.h
+./Limit_C.so: /home/neutron/root/include/TInterpreter.h
+./Limit_C.so: /home/neutron/root/include/TDictionary.h
+./Limit_C.so: /home/neutron/root/include/ESTLType.h
+./Limit_C.so: /home/neutron/root/include/TVirtualMutex.h
+./Limit_C.so: /home/neutron/root/include/TAttCanvas.h
+./Limit_C.so: /home/neutron/root/include/TCanvasImp.h
+./Limit_C.so: /home/neutron/root/include/TROOT.h
+./Limit_C.so: /home/neutron/root/include/TImage.h
+./Limit_C.so: /home/neutron/root/include/TAttImage.h
+./Limit_C.so: /home/neutron/root/include/TSystem.h
+./Limit_C.so: /home/neutron/root/include/TInetAddress.h
+./Limit_C.so: /home/neutron/root/include/TTimer.h
+./Limit_C.so: /home/neutron/root/include/TSysEvtHandler.h
+./Limit_C.so: /home/neutron/root/include/TTime.h
+./Limit_C.so: /home/neutron/root/include/ThreadLocalStorage.h
+./Limit_C.so: /home/neutron/root/include/TStyle.h
+./Limit_C.so: /home/neutron/root/include/TColor.h
+./Limit_C.so: /home/neutron/root/include/TClonesArray.h
+./Limit_C.so: /home/neutron/root/include/TObjArray.h
+./Limit_C.so: /usr/include/c++/6/fstream /usr/include/c++/6/sstream
+./Limit_C.so: ../../../../setNCUStyle.C
+./Limit_C.so: /home/neutron/root/include/RVersion.h /home/neutron/root/include/RConfig.h /home/neutron/root/include/TClass.h /home/neutron/root/include/TDictAttributeMap.h /home/neutron/root/include/TInterpreter.h /home/neutron/root/include/TROOT.h /home/neutron/root/include/TBuffer.h /home/neutron/root/include/TMemberInspector.h /home/neutron/root/include/TError.h /home/neutron/root/include/RtypesImp.h /home/neutron/root/include/TIsAProxy.h /home/neutron/root/include/TFileMergeInfo.h /home/neutron/root/include/TCollectionProxyInfo.h
+Limit_C__ROOTBUILDVERSION= 6.06/06
---
+++ 80x_v6/TMVA/massOpt/Limit.C
@@ -0,0 +1,74 @@
+#include <TLegend.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TRandom.h>
+#include <TLorentzVector.h>
+#include <TFile.h>
+#include <TGraph.h>
+#include <TGraphErrors.h>
+#include <TGraphAsymmErrors.h>
+#include <TH1F.h>
+#include <TH1.h>
+#include <TCanvas.h>
+#include <TROOT.h>
+#include "TImage.h"
+#include "TSystem.h"
+#include "TStyle.h"
+#include <TClonesArray.h>
+#include <fstream>
+#include <cmath>
+#include <TSystem.h>
+#include <string>
+#include <sstream>
+#include"../../../setNCUStyle.C"
+
+TCanvas* c1;
+
+#define  nWidth 4
+#define  nBmin 4
+
+void Limit(){
+	setNCUStyle(true);
+	c1 = new TCanvas("c1","",800,600);
+	
+	int width [nWidth]={25,30,35,40};
+	int bmin[nBmin]={100,105,110,115};
+	string  masspoint[11]={"1000","1200","1400","1600","1800","2000","2500","3000","3500","4000","4500"};
+	
+	TLegend *leg = new TLegend(0.75, 0.68, 0.96, 0.95);
+  
+  leg->SetBorderSize(0);
+  leg->SetFillColor(0);
+  leg->SetFillStyle(0);
+  leg->SetTextSize(0.04);
+  
+  string categoryName[7]={"0a","0c","1a","1c","2a","2b","2d"};
+	
+	for(int h=0;h<7;h++){
+		TFile* tf1=TFile::Open(Form("%s/MassPlotFineBins_subtr_Moriond_Silver105to135.root",categoryName[h].data()));
+		if(h==2) tf1=TFile::Open(Form("%s/MassPlotFineBins_subtr_Moriond_Silver110to145.root",categoryName[h].data()));
+		if(h>=3) tf1=TFile::Open(Form("%s/MassPlotFineBins_subtr_Moriond_Silver115to145.root",categoryName[h].data()));
+			 if (!tf1 || !tf1->IsOpen())continue;
+			 TGraphAsymmErrors* tg1=(TGraphAsymmErrors*)tf1->Get("LimitExpectedCLs");
+		tg1->GetYaxis()->SetTitle("95% CLs on #sigma(X#rightarrowHH)#timesBR(HH#rightarrowb#bar{b}b#bar{b})[fb]");
+			 tg1->SetLineStyle(1);
+			 tg1->SetFillColor(0);
+			 tg1->SetLineColor(h+1);
+			 if (h<2)leg->AddEntry(tg1,Form("%s 105-135",categoryName[h].data()));
+			 else if(h==2)leg->AddEntry(tg1,Form("%s 110-145",categoryName[h].data()));
+			 else if(h>=3)leg->AddEntry(tg1,Form("%s 115-145",categoryName[h].data()));
+			 tg1->SetMaximum(12);
+			 tg1->SetMinimum(0.5);
+			// cout<<k<<","<<m<<endl;
+			 //c1->SetLogy(1);
+			 if(h==1)tg1->Draw("APL");
+			 else tg1->Draw("PL same");
+	}
+  
+	
+	leg->Draw("same");
+	c1->Print("Limit.pdf");
+}
