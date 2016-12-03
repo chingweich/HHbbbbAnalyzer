a9a2aec3798745bc8a2f599995e16ffc201f105a
chingweich <cat8007@gmail.com>
2016-12-04 01:20:38 +0800

    w

--- 80x_v6/TMVA/1203/draw.C
+++ 80x_v6/TMVA/1203/draw.C
@@ -35,6 +35,8 @@ void draw(){
 	c1 = new TCanvas("c1","",1360,768);
 	string  masspoint[11]={"1000","1200","1400","1600","1800","2000","2500","3000","4000","4500"};
 	string massName[3]={"raw","regression","HcorrReg"};
+	
+	/*
 	for(int i=0;i<10;i++){
 		TFile *f;
 	
@@ -110,6 +112,54 @@ void draw(){
 		if(i==0)c1->Print("subleading.pdf(");
 		else if(i==9)c1->Print("subleading.pdf)");
 		else c1->Print("subleading.pdf");
+	}
+	*/
+	for(int i=0;i<10;i++){
+		TFile *f;
+	
+		f=TFile::Open(Form("B%s.root",masspoint[i].data()));
+		TH1D* th1[3];
+		TH1D* th2[3];
+		
+		
+	TLegend *leg = new TLegend(0.15, 0.48, 0.46, 0.88);
+  
+  leg->SetBorderSize(0);
+  leg->SetFillColor(0);
+  leg->SetFillStyle(0);
+  leg->SetTextSize(0.04);
+		
+		//if(i==3)continue;
+		
+		for(int j=0;j<3;j++){
+			//cout<<i<<endl;
+				th1[j]=(TH1D*)f->Get(Form("%s_both",massName[j].data()));
+				//th2[j]=(TH1D*)f->Get(Form("%s_leading",massName[j].data()));
+				
+				//cout<<i<<","<<th2[j]->Integral()<<","<<th1[j]->Integral();
+				//th1[i]->Add(th2[j]);
+				//th1[i]->Scale(1/th1[i]->Integral());
+				th1[j]->SetLineColor(j+1);
+				th1[j]->GetXaxis()->SetRangeUser(50,150);
+				th1[j]->SetTitle(Form("%s",masspoint[i].data()));
+				if(j==0)th1[0]->Draw();
+				else th1[j]->Draw("same");
+				  gStyle->SetOptStat(0);
+		gStyle->SetOptFit(0);
+				TF1 *tf1[4];
+				tf1[0]=new TF1("fa1","gaus(125)",50,150);
+				if(j<1)th1[j]->Fit(tf1[0],"","",70,140);
+				else th1[j]->Fit(tf1[0],"","",85,150);
+				
+				leg->AddEntry(th1[j],Form("%s",massName[j].data()));
+				leg->AddEntry((TObject*)0, Form("Mean=%f",tf1[0]->GetParameter(1)),"");
+				leg->AddEntry((TObject*)0, Form("#sigma=%f",tf1[0]->GetParameter(2)),"");
+		}
+		leg->Draw("same");
+		
+		if(i==0)c1->Print("both.pdf(");
+		else if(i==9)c1->Print("both.pdf)");
+		else c1->Print("both.pdf");
 	}
 	
 	}
