373d971038f7b88809b1bf868a33602be4fb6461
chingweich <cat8007@gmail.com>
2017-02-27 17:44:03 +0800

    check mjj code v2

--- 8022_v1/dbtSF/reducedMassPlots.C
+++ 8022_v1/dbtSF/reducedMassPlots.C
@@ -40,8 +40,16 @@ TH1D* massPlotBase(string input,string variable,double down=100,double up=1000,i
 			Float_t  vari = data.GetFloat(Form("%s",variable.data()));
 			Float_t  jet1bbtag = data.GetFloat("jet1bbtag");
 			Float_t  jet2bbtag = data.GetFloat("jet2bbtag");
+			Float_t  etadiff = data.GetFloat("etadiff");
+			Float_t  jet1_puppi_msoftdrop_TheaCorr = data.GetFloat("jet1_puppi_msoftdrop_TheaCorr");
+			Float_t  jet2_puppi_msoftdrop_TheaCorr = data.GetFloat("jet2_puppi_msoftdrop_TheaCorr");
+			if(jet1_puppi_msoftdrop_TheaCorr<105 ||jet1_puppi_msoftdrop_TheaCorr>135)continue;
+			if(jet2_puppi_msoftdrop_TheaCorr<105 ||jet2_puppi_msoftdrop_TheaCorr>135)continue;
+			if(etadiff>1.3)continue;
 			if((jet1bbtag<0.8 ||jet2bbtag<0.8 )&& category==1)continue;
-			if((jet1bbtag<0.3 ||jet2bbtag<0.3 )&&(!(jet1bbtag>0.8 &&jet2bbtag>0.8 ))&& category==2)continue;
+			if((jet1bbtag<0.3 ||jet2bbtag<0.3 )&& category==2)continue;
+			
+			if((jet1bbtag>0.3 &&jet2bbtag>0.8 )&& category==2)continue;
 			//Float_t  jet2_puppi_msoftdrop = data.GetFloat("jet2_puppi_msoftdrop");
 			th1->Fill(vari);
 	}
@@ -56,17 +64,30 @@ TH1D* massPlotBaseSF(string input,string variable,double down=100,double up=1000
 	tf1->GetObject("mynewTree",tree);
 	TreeReader data(tree);
 				//data.Print();
+	int count=0;
 	for(Long64_t jEntry=0; jEntry<data.GetEntriesFast() ;jEntry++){
 			data.GetEntry(jEntry);
 			Float_t  vari = data.GetFloat(Form("%s",variable.data()));
 			Float_t  jet1bbtag = data.GetFloat("jet1bbtag");
 			Float_t  jet2bbtag = data.GetFloat("jet2bbtag");
-			if((jet1bbtag<0.8 ||jet2bbtag<0.8 )&& category==1)continue;
-			if((jet1bbtag<0.3 ||jet2bbtag<0.3 )&&(!(jet1bbtag>0.8 &&jet2bbtag>0.8 ))&& category==2)continue;
 			Float_t  dbtSF = data.GetFloat("dbtSF");
+			Float_t  etadiff = data.GetFloat("etadiff");
+			Float_t  jet1_puppi_msoftdrop_TheaCorr = data.GetFloat("jet1_puppi_msoftdrop_TheaCorr");
+			Float_t  jet2_puppi_msoftdrop_TheaCorr = data.GetFloat("jet2_puppi_msoftdrop_TheaCorr");
+			if(jet1_puppi_msoftdrop_TheaCorr<105 ||jet1_puppi_msoftdrop_TheaCorr>135)continue;
+			if(jet2_puppi_msoftdrop_TheaCorr<105 ||jet2_puppi_msoftdrop_TheaCorr>135)continue;
+			if(etadiff>1.3)continue;
+			if((jet1bbtag<0.8 ||jet2bbtag<0.8 )&& category==1)continue;
+			if((jet1bbtag<0.3 ||jet2bbtag<0.3 )&& category==2)continue;
+			if((jet1bbtag>0.3 &&jet2bbtag>0.8 )&& category==2)continue;
+			count++;
 			//Float_t  jet2_puppi_msoftdrop = data.GetFloat("jet2_puppi_msoftdrop");
+			if(dbtSF>3||dbtSF<0)continue;
+			if(dbtSF<0.1||dbtSF>2)cout<<"dbtSF="<<dbtSF<<endl;
+			
 			th1->Fill(vari,dbtSF);
 	}
+	//cout<<"count="<<count<<endl;
 	return th1;
 }
 
@@ -125,8 +146,8 @@ void reducedMassPlots(){
 	c1->SetRightMargin(0.01);
 	gStyle->SetNdivisions(605, "XYZ");
 	for(int i=0;i<13;i++){
-		th1[0]=massPlotBase(Form("BulkGrav_M-%d_0.root",massP[i]),"dijetmass_softdrop_corr",massP[i]*0.6,massP[i]*1.2);
-		th1[1]=massPlotBaseSF(Form("BulkGrav_M-%d_0.root",massP[i]),"dijetmass_softdrop_corr",massP[i]*0.6,massP[i]*1.2);
+		th1[0]=massPlotBase(Form("BulkGrav_M-%d_0.root",massP[i]),"dijetmass_softdrop_corr",massP[i]*0.7,massP[i]*1.3);
+		th1[1]=massPlotBaseSF(Form("BulkGrav_M-%d_0.root",massP[i]),"dijetmass_softdrop_corr",massP[i]*0.7,massP[i]*1.3);
 	
 		th1[0]->Draw("");
 		th1[1]->Draw("same");
@@ -141,6 +162,8 @@ void reducedMassPlots(){
 		//th1[1]->SetLineColor(2);
 		c1->Print(Form("reducedMass/TT_grav_%d.pdf",massP[i]));
 		
+		th1[0]->Clear();
+		th1[1]->Clear();
 		th1[0]=massPlotBase(Form("BulkGrav_M-%d_0.root",massP[i]),"dijetmass_softdrop_corr",massP[i]*0.7,massP[i]*1.3,2);
 		th1[1]=massPlotBaseSF(Form("BulkGrav_M-%d_0.root",massP[i]),"dijetmass_softdrop_corr",massP[i]*0.7,massP[i]*1.3,2);
 	
@@ -154,6 +177,7 @@ void reducedMassPlots(){
 		leg->AddEntry(th1[1],"LL dbtSF");
 		leg->Draw("same");
 		//th1[1]->SetLineColor(2);
+		//th1[1]->Draw("");
 		c1->Print(Form("reducedMass/LL_grav_%d.pdf",massP[i]));
 	}
 	
